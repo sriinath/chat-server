fd73c1126b3f43ddcaa2ba157c0c948a37b4899b
srinath <k.srinath@skava.com>
2019-04-01 18:41:08 +0530

    name: srinath chat minor changes

---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'dbURL': 'mongodb+srv://srinath:Srikripa@3@cluster0-oqygq.mongodb.net/test?retryWrites=true',
+    'dbName': 'chat'
+}
--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -1,21 +1,26 @@
 const chatModel = require('../models/chat')
 const chatController = {
+    // get all the user chat list / friends list for a user
     getUserChatList(userName) {
         if(userName) {
             return chatModel.getUserChatList(userName)
         }
-        else {
-            return 'UserName is mandatory'
-        }
+        return 'UserName is mandatory'
     },
+    // get all the chats f that user in a particular group / with a particular person
+    getUserChats(chatId) {
+        if(chatId) {
+            return chatModel.getUserChats(chatId)
+        }
+        return 'ChatId is mandatory'
+    },
+    // add a user chat after adding user as friend
     addUserChat(chatData) {
         const { chatId } = chatData
         if(chatId) {
             return chatModel.addUserChat(chatData)
         }
-        else {
-            return 'ChatId is mandatory'
-        }
+        return 'ChatId is mandatory'
     }
 }
 
